1762af90f97e81a34fc9202cacd681702e090b73
sunzhiqi <sunzhiqi@live.com>
2021-11-22 17:16:03 +0800

    update git clone

--- src/init.js
+++ src/init.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fse = require('fs-extra');
-const download = require('download-git-repo');
+const clone = require('git-clone/promise');
 const spawn = require('cross-spawn')
 const ora = require('ora');
 const spinner = ora();
@@ -15,35 +15,28 @@ const init = (folderName) => {
     fse.removeSync(targetPath);
     fse.ensureDirSync(targetPath);
 
-    new Promise((resolve, reject) => download(
-        "direct:https://github.com/noopn/supos-ccws-template.git#dev",
-        targetPath,
-        { clone: true },
-        resolve
-    )).then((err) => {
-        if (err) {
+    clone('git@github.com:noopn/supos-ccws-template.git',targetPath,{checkout:'dev'})
+        .then((err) => {
+            if (err) throw new Error(err);
+            spinner.succeed('pull supos-ccws-template succeed!');
+            return new Promise((resolve, reject) => {
+                const child = spawn(npm, ['install'], { stdio: 'inherit', cwd: targetPath });
+                child.on('close', code => {
+                    if (code !== 0) {
+                        reject({
+                            command: `error`,
+                        });
+                        return;
+                    }
+                    resolve();
+                });
+            })
+        }).then(() => {
+            spinner.succeed('install dependencies succeed!');
+        }).catch(err => {
             spinner.fail('pull template error， check your network connection!\n', err);
-            process.exit(0);
-        };
-        spinner.succeed('pull supos-ccws-template succeed!');
-        return new Promise((resolve, reject) => {
-            const child = spawn(npm, ['install'], { stdio: 'inherit', cwd: targetPath });
-            child.on('close', code => {
-                if (code !== 0) {
-                    reject({
-                        command: `error`,
-                    });
-                    return;
-                }
-                resolve();
-            });
+            process.exit(0)
         })
-    }).then(res => {
-        spinner.succeed('install dependencies succeed!');
-    }).catch(err => {
-        console.log(err);
-        process.exit(0)
-    })
 }
 
 module.exports = init;
