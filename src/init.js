795359553c42e838822adbb2c7fbe1af91f64de3
sunzhiqi <sunzhiqi@live.com>
2021-11-23 13:54:36 +0800

    fixed npm command

--- src/init.js
+++ src/init.js
@@ -5,8 +5,6 @@ const spawn = require('cross-spawn')
 const ora = require('ora');
 const spinner = ora();
 
-const npm = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
-
 const init = (folderName) => {
     const cwd = process.cwd();
     const targetPath = path.join(cwd, folderName);
@@ -20,7 +18,7 @@ const init = (folderName) => {
             if (err) throw new Error(err);
             spinner.succeed('pull supos-ccws-template succeed!');
             return new Promise((resolve, reject) => {
-                const child = spawn(npm, ['install'], { stdio: 'inherit', cwd: targetPath });
+                const child = spawn('npm', ['install'], { stdio: 'inherit', cwd: targetPath });
                 child.on('close', code => {
                     if (code !== 0) {
                         reject({
