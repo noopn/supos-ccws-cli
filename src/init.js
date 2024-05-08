781d818f3e8919faaa6878dbebe4059135c9f24a
sunzhiqi <sunzhiqi@live.com>
2024-05-08 14:48:29 +0800

    chore

--- src/init.js
+++ src/init.js
@@ -3,11 +3,20 @@ const fse = require("fs-extra");
 const spawn = require("cross-spawn");
 const inquirer = require("inquirer");
 
+const cwd = process.cwd();
+
 const useYarn = spawn.sync("yarn", ["--version"]).stdout.toString();
 
+const isInWorkspace = !!fse.pathExistsSync(path.join(cwd, "package.json"));
+
+if (isInWorkspace) {
+  console.log("you are already in a project, please select another directory");
+  process.exit(1);
+}
+
 const init = async (folderName) => {
-  const cwd = process.cwd();
   const targetPath = path.join(cwd, folderName);
+
   if (fse.pathExistsSync(targetPath)) {
     await inquirer
       .prompt([
@@ -28,29 +37,44 @@ const init = async (folderName) => {
 
   fse.mkdirSync(targetPath);
 
-  console.log("install dependencies");
-  console.log();
+  console.log("start init workspace");
 
   const template = "supos-ccws-template";
+  const scripts = "supos-ccws-scripts";
 
-  const ver = spawn.sync("npm", ["show", template, "version"], {
+  const tplStd = spawn.sync("npm", ["show", template, "version"], {
     encoding: "utf8",
   });
 
-  if (ver.stderr) {
-    process.stdout.write(ver.stderr);
+  if (tplStd.stderr) {
+    process.stdout.write(tplStd.stderr);
     process.exit(1);
   }
 
-  const version = ver.stdout.split("/")[0];
+  const scriptsStd = spawn.sync("npm", ["show", scripts, "version"], {
+    encoding: "utf8",
+  });
+
+  if (scriptsStd.stderr) {
+    process.stdout.write(scriptsStd.stderr);
+    process.exit(1);
+  }
+
+  const templateVersion = tplStd.stdout.split(/\n/)[0];
+
+  const scriptsVersion = scriptsStd.stdout.split(/\n/)[0];
 
   const command = useYarn ? "yarn" : "npm";
 
-  let args = [useYarn ? "add" : "install", `${template}@^${version}`];
-
-  if (!useYarn) {
-    args.push("--save");
-  }
+  let args = [
+    useYarn ? "add" : "install",
+    `${template}@^${templateVersion}`,
+    `${scripts}@^${scriptsVersion}`,
+  ];
+  console.clear();
+  console.log(
+    `install dependencies ${template}@^${templateVersion} ${scripts}@^${scriptsVersion}`
+  );
 
   let result = spawn.sync(command, args, {
     stdio: "inherit",
@@ -66,15 +90,53 @@ const init = async (folderName) => {
   console.log();
 
   const sourcePath = path.join(targetPath, "node_modules", template);
-  fse.copySync(sourcePath, targetPath);
 
-  args = [useYarn ? "remove" : "uninstall", "--silent", template];
+  fse.copySync(path.join(sourcePath, "src"), path.join(targetPath, "src"));
+  fse.copyFileSync(
+    path.join(sourcePath, "ccws.config.json"),
+    path.join(targetPath, "ccws.config.json")
+  );
+  fse.copyFileSync(
+    path.join(sourcePath, "tsconfig.json"),
+    path.join(targetPath, "tsconfig.json")
+  );
+  fse.copyFileSync(
+    path.join(sourcePath, "README.md"),
+    path.join(targetPath, "README.md")
+  );
 
-  result = spawn.sync(command, args, { stdio: "inherit", cwd: targetPath });
+  spawn.sync(
+    command,
+    [useYarn ? "remove" : "uninstall", template, "--silent"],
+    {
+      stdio: "inherit",
+      cwd: targetPath,
+    }
+  );
 
-  if (result.signal) {
-    process.exit(1);
-  }
+  const packageJson = {
+    name: "supos-ccws-workspace",
+    version: "1.0.0",
+    license: "MIT",
+    keywords: ["supos", "component workspace"],
+    private: true,
+
+    scripts: {
+      pull: "supos-ccws-scripts pull --ignore-dependencies",
+      "pull:deps": "supos-ccws-scripts pull",
+      dev: "supos-ccws-scripts dev",
+      build: "supos-ccws-scripts build",
+      push: "supos-ccws-scripts push",
+    },
+    dependencies: {
+      [scripts]: `^${scriptsVersion}`,
+    },
+  };
+
+  fse.writeFileSync(
+    path.join(targetPath, "package.json"),
+    JSON.stringify(packageJson, null, 2)
+  );
 
   console.log("supos-ccws-template init success!");
 };
